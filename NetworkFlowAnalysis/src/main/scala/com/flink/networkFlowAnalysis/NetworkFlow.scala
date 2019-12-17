d266f7de44145994338b73dae2f817aecb0dfee4
james <james@JamesUbuntu>
2019-12-17 08:02:43 +0800

    update

--- NetworkFlowAnalysis/src/main/scala/com/flink/networkFlowAnalysis/NetworkFlow.scala
+++ NetworkFlowAnalysis/src/main/scala/com/flink/networkFlowAnalysis/NetworkFlow.scala
@@ -16,15 +16,16 @@ import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
 
 import scala.collection.mutable.ListBuffer
+
 // 输入数据样例类
-case class ApacheLogEvent(ip:String,userId:String,eventTime:Long,method:String,url:String)
+case class ApacheLogEvent(ip: String, userId: String, eventTime: Long, method: String, url: String)
 
 // 窗口聚合结果样例类
-case class UrlViewCount(url:String,windowEnd:Long,count:Long)
+case class UrlViewCount(url: String, windowEnd: Long, count: Long)
 
 
 /**
-  *  热门页面流量统计
+  * 热门页面流量统计
   */
 object NetworkFlow {
   def main(args: Array[String]): Unit = {
@@ -33,20 +34,20 @@ object NetworkFlow {
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     val dataStream = env.readTextFile("C:\\Users\\lenovo-aa\\Desktop\\flink\\flink-UserBehaviorAnalysis\\userBehaviorAnalysis\\NetworkFlowAnalysis\\src\\main\\resources\\apache.log")
-      .map(data=>{
+      .map(data => {
         val dataArray = data.split(" ")
         // 定义时间转换
         val simplmeDateFormat = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss")
         val timestamp = simplmeDateFormat.parse(dataArray(3).trim).getTime
-        ApacheLogEvent(dataArray(0).trim,dataArray(1).trim,timestamp,dataArray(5).trim,dataArray(6).trim)
+        ApacheLogEvent(dataArray(0).trim, dataArray(1).trim, timestamp, dataArray(5).trim, dataArray(6).trim)
       })
       .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[ApacheLogEvent](Time.seconds(1)) {
         override def extractTimestamp(element: ApacheLogEvent): Long = element.eventTime //ms
       })
       .keyBy(_.url)
-      .timeWindow(Time.minutes(10),Time.seconds(5))
+      .timeWindow(Time.minutes(10), Time.seconds(5))
       .allowedLateness(Time.seconds(60)) // 允许60s迟到数据
-      .aggregate(new CountAgg(),new WindowResult())
+      .aggregate(new CountAgg(), new WindowResult())
       .keyBy(_.windowEnd)
       .process(new TopNHotUrls(5))
 
@@ -58,46 +59,49 @@ object NetworkFlow {
 }
 
 // 自定义预聚合函数
-class CountAgg() extends AggregateFunction[ApacheLogEvent,Long,Long]{
+class CountAgg() extends AggregateFunction[ApacheLogEvent, Long, Long] {
   override def createAccumulator(): Long = 0L
-  override def add(value: ApacheLogEvent, accumulator: Long): Long = accumulator+1
+
+  override def add(value: ApacheLogEvent, accumulator: Long): Long = accumulator + 1
+
   override def getResult(accumulator: Long): Long = accumulator
-  override def merge(a: Long, b: Long): Long = a+b
+
+  override def merge(a: Long, b: Long): Long = a + b
 }
 
 // 自定义窗口处理函数
-class WindowResult() extends WindowFunction[Long,UrlViewCount,String,TimeWindow]{
+class WindowResult() extends WindowFunction[Long, UrlViewCount, String, TimeWindow] {
   override def apply(key: String, window: TimeWindow, input: Iterable[Long], out: Collector[UrlViewCount]): Unit = {
-    out.collect(UrlViewCount(key,window.getEnd,input.iterator.next()))
+    out.collect(UrlViewCount(key, window.getEnd, input.iterator.next()))
   }
 }
 
 // 自定义排序输出处理函数
-class TopNHotUrls(topSize:Int) extends KeyedProcessFunction[Long,UrlViewCount,String]{
-  lazy val urlState:ListState[UrlViewCount] = getRuntimeContext.getListState(new ListStateDescriptor[UrlViewCount]("url-state",classOf[UrlViewCount]))
+class TopNHotUrls(topSize: Int) extends KeyedProcessFunction[Long, UrlViewCount, String] {
+  lazy val urlState: ListState[UrlViewCount] = getRuntimeContext.getListState(new ListStateDescriptor[UrlViewCount]("url-state", classOf[UrlViewCount]))
 
   override def processElement(value: UrlViewCount, ctx: KeyedProcessFunction[Long, UrlViewCount, String]#Context, out: Collector[String]): Unit = {
     urlState.add(value)
-    ctx.timerService().registerEventTimeTimer(value.windowEnd+1)
+    ctx.timerService().registerEventTimeTimer(value.windowEnd + 1)
   }
 
   override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, UrlViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
     // 从状态中拿到数据
-    val allUrlViews:ListBuffer[UrlViewCount] =new ListBuffer[UrlViewCount]()
+    val allUrlViews: ListBuffer[UrlViewCount] = new ListBuffer[UrlViewCount]()
     val iter = urlState.get().iterator()
-    while(iter.hasNext){
-      allUrlViews+=iter.next()
+    while (iter.hasNext) {
+      allUrlViews += iter.next()
     }
     urlState.clear()
 
-    val sortedUrlViews=allUrlViews.sortWith(_.count>_.count).take(topSize)
+    val sortedUrlViews = allUrlViews.sortWith(_.count > _.count).take(topSize)
 
     // 格式化结果输出
-    val result:StringBuilder = new StringBuilder()
-    result.append("时间：").append(new Timestamp(timestamp-1)).append("\n")
-    for(i<- sortedUrlViews.indices){
+    val result: StringBuilder = new StringBuilder()
+    result.append("时间：").append(new Timestamp(timestamp - 1)).append("\n")
+    for (i <- sortedUrlViews.indices) {
       val currentUrlView = sortedUrlViews(i)
-      result.append("No.").append(i+1).append(":")
+      result.append("No.").append(i + 1).append(":")
         .append(" URL=").append(currentUrlView.url)
         .append(" 访问量=").append(currentUrlView.count).append("\n")
     }
--- NetworkFlowAnalysis/src/main/scala/com/flink/pageView/PageView.scala
+++ NetworkFlowAnalysis/src/main/scala/com/flink/pageView/PageView.scala
@@ -8,11 +8,11 @@ import org.apache.flink.streaming.api.windowing.time.Time
 
 /**
   *
-  *  统计整个网站数据中页面浏览量或点击量
+  * 统计整个网站数据中页面浏览量或点击量
   *
   */
 // 先定义输入数据的样例类
-case class UserBehavior(userId:Long,itemId:Long,categoryId:Int,behavior:String,timestamp:Long)
+case class UserBehavior(userId: Long, itemId: Long, categoryId: Int, behavior: String, timestamp: Long)
 
 
 object PageView {
@@ -24,26 +24,23 @@ object PageView {
     // 用相对路径定义数据源
     val resource = getClass.getResource("/UserBehavior.csv")
     val dataStream = env.readTextFile(resource.getPath)
-      .map(data =>{
+      .map(data => {
         val dataArray = data.split(",")
-        UserBehavior(dataArray(0).trim.toLong,dataArray(1).trim.toLong,dataArray(2).trim.toInt,dataArray(3).trim,dataArray(4).trim.toLong)
+        UserBehavior(dataArray(0).trim.toLong, dataArray(1).trim.toLong, dataArray(2).trim.toInt, dataArray(3).trim, dataArray(4).trim.toLong)
       })
       .assignAscendingTimestamps(_.timestamp * 1000L) // 升序数据，直接分配时间戳
-      .filter(_.behavior=="pv")  // 只统计pv操作
-      .map(data=>("pv",1))
-//      .keyBy(_._1)  // 哑key
-//      .timeWindow(Time.hours(1))
-//      .sum(1)
-        .windowAll(TumblingEventTimeWindows.of(Time.hours(1)))  // 先用哑key可以
-        .sum(1)
-
+      .filter(_.behavior == "pv") // 只统计pv操作
+      .map(data => ("pv", 1))
+      //      .keyBy(_._1)  // 哑key
+      //      .timeWindow(Time.hours(1))
+      //      .sum(1)
+      .windowAll(TumblingEventTimeWindows.of(Time.hours(1))) // 先用哑key可以
+      .sum(1)
 
 
     dataStream.print("pv count")
 
     env.execute("page view job")
-
-
 
 
   }
