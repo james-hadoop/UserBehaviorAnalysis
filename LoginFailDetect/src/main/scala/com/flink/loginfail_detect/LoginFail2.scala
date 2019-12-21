dbb41102b0ed74c686767dd0b14f36a824e90208
james <james@JamesUbuntu>
2019-12-21 15:12:51 +0800

    update

--- LoginFailDetect/src/main/scala/com/flink/loginfail_detect/LoginFail2.scala
+++ LoginFailDetect/src/main/scala/com/flink/loginfail_detect/LoginFail2.scala
@@ -16,7 +16,7 @@ case class LoginEvent(userId: Long, ip: String, eventType: String, eventTime: Lo
 // 输出的一场报警信息样例类
 case class Warning(userId: Long, firstFailTime: Long, lastFailTime: Long, warningMsg: String)
 
-object LoginFail {
+object LoginFail2 {
   def main(args: Array[String]): Unit = {
     //获取运行环境
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
@@ -52,36 +52,59 @@ class LoginWarning(maxFailTimes: Int) extends KeyedProcessFunction[Long, LoginEv
   lazy val logFailState: ListState[LoginEvent] = getRuntimeContext.getListState(new ListStateDescriptor[LoginEvent]("login-fail-state", classOf[LoginEvent]))
 
   override def processElement(i: LoginEvent, context: KeyedProcessFunction[Long, LoginEvent, Warning]#Context, collector: Collector[Warning]): Unit = {
-    val loginFailList = logFailState.get()
+    //    val loginFailList = logFailState.get()
+    //
+    //    // 判度类型是否fail，只添加fail的事件到状态
+    //    if (i.eventType == "fail") {
+    //      if (loginFailList.iterator().hasNext) {
+    //        context.timerService().registerEventTimeTimer(i.eventTime * 1000L + 2000L)
+    //      }
+    //
+    //      logFailState.add(i)
+    //    } else {
+    //      //如果成功，清空状态
+    //      logFailState.clear()
+    //    }
 
-    // 判度类型是否fail，只添加fail的事件到状态
     if (i.eventType == "fail") {
-      if (loginFailList.iterator().hasNext) {
-        context.timerService().registerEventTimeTimer(i.eventTime * 1000L + 2000L)
-      }
+      // 如果失败，判断之前是否有登录失败事件
+      val iter = logFailState.get().iterator()
+      if (iter.hasNext) {
+        //如果已经有登录失败事件，就比较事件时间
+        val firstFail = iter.next()
+        if (i.eventTime < firstFail.eventTime + 2) {
+          //如果两次间隔小于2秒，输出报警
+          collector.collect(Warning(i.userId, firstFail.eventTime, i.eventTime, "login fail in 2 seconds."))
+        }
 
-      logFailState.add(i)
+        // 更新最近一次的登录失败事件，保存在状态里
+        logFailState.clear()
+        logFailState.add(i)
+      } else {
+        //如果是第一次登录失败，直接添加到状态
+        logFailState.add(i)
+      }
     } else {
-      //如果成功，清空状态
+      // 如果成功，清空状态
       logFailState.clear()
     }
   }
 
-  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, LoginEvent, Warning]#OnTimerContext, out: Collector[Warning]): Unit = {
-    // 触发定时器的时候，根据状态李的失败个数决定是否输出报警
-    val allLoginFails: ListBuffer[LoginEvent] = new ListBuffer[LoginEvent]()
-    val iter = logFailState.get().iterator()
-    while (iter.hasNext) {
-      allLoginFails += iter.next()
-    }
-
-    // 判断个数
-    if (allLoginFails.length >= maxFailTimes) {
-      out.collect(Warning(allLoginFails.head.userId, allLoginFails.head.eventTime, allLoginFails.last.eventTime, "login fail in 2 seconds for " + allLoginFails.length + " times."))
-
-    }
-
-    // 清空状态
-    logFailState.clear()
-  }
+  //  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, LoginEvent, Warning]#OnTimerContext, out: Collector[Warning]): Unit = {
+  //    // 触发定时器的时候，根据状态李的失败个数决定是否输出报警
+  //    val allLoginFails: ListBuffer[LoginEvent] = new ListBuffer[LoginEvent]()
+  //    val iter = logFailState.get().iterator()
+  //    while (iter.hasNext) {
+  //      allLoginFails += iter.next()
+  //    }
+  //
+  //    // 判断个数
+  //    if (allLoginFails.length >= maxFailTimes) {
+  //      out.collect(Warning(allLoginFails.head.userId, allLoginFails.head.eventTime, allLoginFails.last.eventTime, "login fail in 2 seconds for " + allLoginFails.length + " times."))
+  //
+  //    }
+  //
+  //    // 清空状态
+  //    logFailState.clear()
+  //  }
 }
